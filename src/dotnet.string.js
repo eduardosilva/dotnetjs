dafc4431cab3781ed59060150c07b40ad08b22ec
eduardo-silva <eduardo.lour.silva@gmail.com>
2016-08-24 17:24:47 -0300

    Added new features to string type

--- src/dotnet.string.js
+++ src/dotnet.string.js
@@ -1,3 +1,20 @@
-﻿String.prototype.padLeft = function(length, char) {
+﻿String.Join = function (array, separator){
+	var result = "";
+  
+  array.forEach(function (i) {
+  	if (result !== "")
+    	result += separator;
+      
+		result += i;
+  });
+  
+  return result;
+}
+
+String.isNullOrEmpty = function (value) {
+	return !value || value === "";
+};
+
+String.prototype.padLeft = function(length, char) {
   return Array(length - this.length + 1).join(char || " ") + this;
 };
