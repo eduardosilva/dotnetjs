f2175ceb3e86d8c6588f2d9848e32cea15a55b24
Eduardo Silva <eduardo.lour.silva@gmail.com>
2016-12-06 13:57:27 -0200

    added sum method

--- src/dotnet.array.js
+++ src/dotnet.array.js
@@ -78,3 +78,64 @@ Array.prototype.where = function(predicate) {
   }
   return result;
 };
+
+Array.prototype.count = function (predicate) {
+    return this.where(predicate).length;
+};
+Array.prototype.selectMany = function (func) {
+    var results = [];
+    for (j = 0, len = this.length; j < len; j++) {
+        i = this[j];
+        func(i).forEach(function (ii) { results.push(ii); });
+    }
+
+    return results;
+};
+
+Array.prototype.min = function (func) {
+    var result = undefined;
+
+    for (j = 0, len = this.length; j < len; j++) {
+        i = this[j];
+        var currentValue = func(i);
+
+        if (result == undefined || currentValue < result)
+            result = currentValue;
+    }
+
+    return result;
+};
+
+Array.prototype.max = function (func) {
+    var result = undefined;
+
+    for (j = 0, len = this.length; j < len; j++) {
+        i = this[j];
+        var currentValue = func(i);
+
+        if (result == undefined || currentValue > result)
+            result = currentValue;
+    }
+
+    return result;
+};
+
+Array.prototype.remove = function (item) {
+    for (i = 0, len = this.length; i < len; i++) {
+        if (item === this[i])
+        {
+            this.splice(i, 1);
+            break;
+        }
+    }
+};
+
+Array.prototype.sum = function (func){
+	var result = 0;
+	for (i = 0, len = this.length; i < len; i++) {
+		
+		result += func ? func(this[i]) : this[i];
+	}
+	
+	return result;
+}
